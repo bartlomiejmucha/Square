9486106200de94358c80e326081567686edd5a2b
Bartłomiej Mucha <bartlomiej.mucha@hotmail.com>
2018-08-31 14:06:38 +0200

    install scripts upgraded to sitecore 9.0 update-2

--- install/parameters.ps1
+++ install/parameters.ps1
@@ -27,8 +27,8 @@ $srcZipsPath = "$srcInstallScriptsPath\zips"
 
 #source zip
 $srcSolrZipPath = "$srcZipsPath\solr-6.6.2.zip"
-$srcXConnectWDPZipPath = "$srcZipsPath\Sitecore 9.0.1 rev. 171219 (OnPrem)_xp0xconnect.scwdp.zip"
-$srcSitecoreWDPZipPath = "$srcZipsPath\Sitecore 9.0.1 rev. 171219 (OnPrem)_single.scwdp.zip"
+$srcXConnectWDPZipPath = "$srcZipsPath\Sitecore 9.0.2 rev. 180604 (OnPrem)_xp0xconnect.scwdp.zip"
+$srcSitecoreWDPZipPath = "$srcZipsPath\Sitecore 9.0.2 rev. 180604 (OnPrem)_single.scwdp.zip"
 
 #destination solr
 $destSolrPath = "$installRootPath\solr"
